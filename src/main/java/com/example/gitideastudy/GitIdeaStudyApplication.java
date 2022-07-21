c47b606fb375ef5707508b68311a060b91b5ac3b
shen <873631356@qq.com>
2022-07-21 21:29:53 +0800

    Initial commit

---
+++ src/main/java/com/example/gitideastudy/GitIdeaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitideastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIdeaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitIdeaStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitideastudy/GitIdeaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitideastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIdeaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
